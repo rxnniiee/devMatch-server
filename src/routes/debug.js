dc1b9914e931b500f47da68340c2e2a257335e6f
rxnniiee <23708969+rxnniiee@users.noreply.github.com>
2021-05-21 04:05:51 +0300

    protect debug route to any user

--- src/routes/debug.js
+++ src/routes/debug.js
@@ -1,9 +1,9 @@
 const router = require('express').Router()
-
+const authorization = require ('../middlewares/authorization')
 const debugController = require('../controllers/debugController')
 
-router.get('/talents', debugController.talents)
-router.get('/employers', debugController.employers)
+router.get('/talents', authorization, debugController.talents)
+router.get('/employers', authorization, debugController.employers)
 
 module.exports = router
 module.exports.meta = {
